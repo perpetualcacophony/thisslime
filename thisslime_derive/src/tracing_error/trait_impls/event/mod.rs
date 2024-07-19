065c94a6c2ea86c19debb787d404beec40de384f
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-07-19 14:45:52 -0500

    macro for event impl

--- thisslime_derive/src/tracing_error/trait_impls/event/mod.rs
+++ thisslime_derive/src/tracing_error/trait_impls/event/mod.rs
@@ -1,12 +1,4 @@
-mod methods;
-
-pub fn quote(input: &syn::DeriveInput) -> syn::Result<syn::ItemImpl> {
-    let ident = &input.ident;
-    let methods = methods::list(input)?;
-
-    Ok(syn::parse_quote!(
-        impl ::thisslime::tracing::Event for #ident {
-            #(#methods)*
-        }
-    ))
+impl_trait! {
+    ::thisslime::tracing::Event=>
+    [fn construct()]
 }
