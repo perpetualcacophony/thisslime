d1e2452f63af89e4dd8950aaf248f50c2a2510e1
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-07-19 17:22:49 -0500

    added script to bump version

---
+++ scripts/bump_version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+MAJOR=$(sed -En 's|workspace.package.version = "([0-9]+).[0-9]+.[0-9]+"|\1|p' ./Cargo.toml)
+MINOR=$(sed -En 's|workspace.package.version = "[0-9]+.([0-9]+).[0-9]+"|\1|p' ./Cargo.toml)
+PATCH=$(sed -En 's|workspace.package.version = "[0-9]+.[0-9]+.([0-9]+)"|\1|p' ./Cargo.toml)
+
+if [[ "$1" == "minor" ]]; then
+    ((MINOR++))
+    PATCH=0
+else
+    PATCH=$((PATCH+1))
+fi
+
+VERSION="$MAJOR.$MINOR.$PATCH"
+
+./scripts/set_version.sh $VERSION
+
+echo $VERSION
--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -3,4 +3,4 @@ set -e
 
 sed -Ei "s|workspace.package.version = \"(.*)\"|workspace.package.version = \"$1\"|g" ./Cargo.toml
 
-./fix_versions.sh
+./scripts/fix_versions.sh
