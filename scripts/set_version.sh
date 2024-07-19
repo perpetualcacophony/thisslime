e3d2881affeba0913b75a29dedc8ba96bb95196c
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-07-19 18:26:15 -0500

    fixed set version script

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sed -Ei "s|workspace.package.version = \"(.*)\"|workspace.package.version = \"$1\"|g" ./Cargo.toml
+sed -Ei "s|version = \"(.*)\"|version = \"$1\"|g" ./Cargo.toml
 
 ./scripts/fix_versions.sh
