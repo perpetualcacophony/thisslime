6c4e26a56961d72dfb205342c7ccaa59e9d02903
kate <142743629+perpetualcacophony@users.noreply.github.com>
2024-07-19 17:57:46 -0500

    made the toml stuff marginally less ugly

--- scripts/fix_versions.sh
+++ scripts/fix_versions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-VERSION=$(sed -En 's|workspace.package.version = "(.*)"|\1|p' ./Cargo.toml)
+VERSION=$(sed -En 's|version = "(.*)"|\1|p' ./Cargo.toml)
 
 function fix_dependencies () {
-    sed -Ei "s|(dependencies.thisslime-.+.version =)(.*)|\1 \"=$VERSION\"|g" ./$1/Cargo.toml
+    sed -Ei "s|(thisslime-.+.version =)(.*)|\1 \"=$VERSION\"|g" ./$1/Cargo.toml
 }
 
 fix_dependencies thisslime-derive
